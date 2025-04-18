3b14700ad79315de414012d93e3508a86fca4ec3
Peter <158618672+Satturas@users.noreply.github.com>
2025-04-18 23:33:19 +0300

    Revert "Dev common"

--- app/src/main/java/com/example/my_shoplist_application/domain/api/ShoplistScreenInteractor.kt
+++
@@ -1,7 +0,0 @@
-package com.example.my_shoplist_application.domain.api
-
-import com.example.my_shoplist_application.domain.models.Shoplist
-
-interface ShoplistScreenInteractor {
-    fun createShoplist(shoplist: Shoplist)
-}
--- app/src/main/java/com/example/my_shoplist_application/domain/db/ShoplistScreenRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.example.my_shoplist_application.domain.db
-
-import com.example.my_shoplist_application.domain.models.Shoplist
-
-interface ShoplistScreenRepository {
-    fun createShoplist(shoplist: Shoplist)
-}
