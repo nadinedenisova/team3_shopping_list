7df681ff120f3f92dbeb585f756f0748d7cea142
Peter <klintsov.p@mail.ru>
2025-04-18 23:54:49 +0300

    создана viewmodel для экрана списка продуктов

--- app/src/main/java/com/example/my_shoplist_application/presentation/model/MainScreenState.kt
+++
@@ -1,5 +0,0 @@
-package com.example.my_shoplist_application.presentation.model
-
-sealed interface MainScreenState {
-    data object Default : MainScreenState
-}
--- app/src/main/java/com/example/my_shoplist_application/presentation/model/ShoplistScreenState.kt
+++
@@ -1,7 +0,0 @@
-package com.example.my_shoplist_application.presentation.model
-
-sealed interface ShoplistScreenState {
-    data object NewList: ShoplistScreenState
-    data object EditList: ShoplistScreenState
-    data object AddingIngridient: ShoplistScreenState
-}
